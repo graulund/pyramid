d95f7f358d528ee2a118b683264413efdccdb22b
Andy Graulund <andreas@graulund.com>
2017-04-08 21:05:55 +0200

    Emoticon image replacements courtesy of FFZ

--- public/src/js/twitch/TwitchMessageLine.jsx
+++ public/src/js/twitch/TwitchMessageLine.jsx
@@ -8,6 +8,24 @@ const EMOTE_IMG_URL_ROOT = "//static-cdn.jtvnw.net/emoticons/v1/";
 const EMOTE_FFZ_IMG_URL_ROOT = "//cdn.frankerfacez.com/emoticon/";
 const EMOTE_BTTV_IMG_URL_ROOT = "//cdn.betterttv.net/emote/";
 
+const EMOTE_FFZ_REPLACEMENT_ROOT = "//cdn.frankerfacez.com/script/replacements/";
+
+const EMOTE_REPLACEMENTS = {
+	15: EMOTE_FFZ_REPLACEMENT_ROOT + "15-JKanStyle.png",
+	16: EMOTE_FFZ_REPLACEMENT_ROOT + "16-OptimizePrime.png",
+	17: EMOTE_FFZ_REPLACEMENT_ROOT + "17-StoneLightning.png",
+	18: EMOTE_FFZ_REPLACEMENT_ROOT + "18-TheRinger.png",
+	19: EMOTE_FFZ_REPLACEMENT_ROOT + "19-PazPazowitz.png",
+	20: EMOTE_FFZ_REPLACEMENT_ROOT + "20-EagleEye.png",
+	21: EMOTE_FFZ_REPLACEMENT_ROOT + "21-CougarHunt.png",
+	22: EMOTE_FFZ_REPLACEMENT_ROOT + "22-RedCoat.png",
+	26: EMOTE_FFZ_REPLACEMENT_ROOT + "26-JonCarnage.png",
+	27: EMOTE_FFZ_REPLACEMENT_ROOT + "27-PicoMause.png",
+	30: EMOTE_FFZ_REPLACEMENT_ROOT + "30-BCWarrior.png",
+	33: EMOTE_FFZ_REPLACEMENT_ROOT + "33-DansGame.png",
+	36: EMOTE_FFZ_REPLACEMENT_ROOT + "36-PJSalt.png"
+};
+
 const getEmoticonUrlsets = (emote) => {
 	const output = {};
 	switch (emote.type) {
@@ -33,12 +51,18 @@ const getEmoticonUrlsets = (emote) => {
 			break;
 		default:
 			// Assume normal
-			output.src = EMOTE_IMG_URL_ROOT + emote.id + "/1.0";
-			output.srcSet = [
-				EMOTE_IMG_URL_ROOT + emote.id + "/1.0 1x",
-				EMOTE_IMG_URL_ROOT + emote.id + "/2.0 2x",
-				EMOTE_IMG_URL_ROOT + emote.id + "/3.0 3x"
-			];
+			if (emote.id in EMOTE_REPLACEMENTS) {
+				output.src = EMOTE_REPLACEMENTS[emote.id];
+				output.srcSet = [output.src + " 1x"];
+			}
+			else {
+				output.src = EMOTE_IMG_URL_ROOT + emote.id + "/1.0";
+				output.srcSet = [
+					EMOTE_IMG_URL_ROOT + emote.id + "/1.0 1x",
+					EMOTE_IMG_URL_ROOT + emote.id + "/2.0 2x",
+					EMOTE_IMG_URL_ROOT + emote.id + "/3.0 3x"
+				];
+			}
 	}
 
 	return output;
