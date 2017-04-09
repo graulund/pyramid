d413534bc2548f95e896a62929f247220787a96a
Andy Graulund <andreas@graulund.com>
2017-04-09 13:06:08 +0200

    More style tweaks

--- public/src/js/settingsview/SettingsView.jsx
+++ public/src/js/settingsview/SettingsView.jsx
@@ -37,29 +37,31 @@ class SettingsView extends PureComponent {
 		return (
 			<div className={className}>
 				<div className="mainview__top settingsview__top">
-					<h2>Settings</h2>
-					<ul className="settingsview__tabs switcher" key="tabs">
-						<li key="general">
-							<Link className="general" to={settingsUrl()}>
-								General
-							</Link>
-						</li>
-						<li key="friends">
-							<Link className="friends" to={settingsUrl("friends")}>
-								Friends
-							</Link>
-						</li>
-						<li key="irc">
-							<Link className="irc" to={settingsUrl("irc")}>
-								IRC
-							</Link>
-						</li>
-						<li key="nicknames">
-							<Link className="nicknames" to={settingsUrl("nicknames")}>
-								Nicknames
-							</Link>
-						</li>
-					</ul>
+					<div className="mainview__top__main">
+						<h2>Settings</h2>
+						<ul className="settingsview__tabs switcher" key="tabs">
+							<li key="general">
+								<Link className="general" to={settingsUrl()}>
+									General
+								</Link>
+							</li>
+							<li key="friends">
+								<Link className="friends" to={settingsUrl("friends")}>
+									Friends
+								</Link>
+							</li>
+							<li key="irc">
+								<Link className="irc" to={settingsUrl("irc")}>
+									IRC
+								</Link>
+							</li>
+							<li key="nicknames">
+								<Link className="nicknames" to={settingsUrl("nicknames")}>
+									Nicknames
+								</Link>
+							</li>
+						</ul>
+					</div>
 				</div>
 				{ page }
 			</div>
