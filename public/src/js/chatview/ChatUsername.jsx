b2f32b9506520430fff422ef396c63943b2d5240
Andy Graulund <andreas@graulund.com>
2017-04-14 11:23:24 +0200

    Migrate to prop-types package

--- public/src/js/chatview/ChatUsername.jsx
+++ public/src/js/chatview/ChatUsername.jsx
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from "react";
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { TWITCH_DISPLAY_NAMES } from "../constants";
