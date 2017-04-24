10f45522832f01c1b9067292abd30b2ff0394b1f
Andy Graulund <andreas@graulund.com>
2017-04-24 23:21:00 +0200

    Simplifying Header component

--- public/src/js/components/Header.jsx
+++ public/src/js/components/Header.jsx
@@ -1,27 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
 import store from "../store";
 import actions from "../actions";
 
-class Header extends Component {
-	constructor(props) {
-		super(props);
-		this.openSidebar = this.openSidebar.bind(this);
-	}
-
-	openSidebar() {
-		store.dispatch(actions.viewState.update({ sidebarVisible: true }));
-	}
-
-	render() {
-		return (
-			<div id="header">
-				<a className="sidebar__open" href="javascript://" onClick={this.openSidebar}>
-					<img src="/img/menu.svg" width="16" height="16" alt="Sidebar" />
-				</a>
-			</div>
-		);
-	}
+function openSidebar() {
+	store.dispatch(actions.viewState.update({ sidebarVisible: true }));
 }
+
+const Header = function() {
+	return (
+		<div id="header">
+			<a
+				className="sidebar__open"
+				href="javascript://"
+				onClick={openSidebar}>
+				<img src="/img/menu.svg" width="16" height="16" alt="Sidebar" />
+			</a>
+		</div>
+	);
+};
 
 export default Header;
