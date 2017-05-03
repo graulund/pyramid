dd8307d3e81c73e261c789677632c25517f9a740
Andy Graulund <andreas@graulund.com>
2017-05-03 23:47:07 +0200

    Explain some more

--- scripts/updateDbIndexes.js
+++ scripts/updateDbIndexes.js
@@ -7,18 +7,44 @@ var db;
 
 dbSource({ setDb: (_) => { db = _; } }, () => {
 
-	const callback = (err, data) => {
-		if (err) {
-			throw err;
-		}
+	const callback = function(message) {
+		return function(err, data) {
+			if (err) {
+				console.error("Error: " + message);
+				//throw err;
+			}
+			else {
+				console.log("Success: " + message);
+			}
+		};
 	};
 
+	console.log("This could take a while... hold on.");
+
 	db._db.serialize(function() {
-		db._db.run('DROP INDEX "username"', callback);
-		db._db.run('DROP INDEX "date"', callback);
-		db._db.run('DROP INDEX "channelId"', callback);
-		db._db.run('CREATE INDEX "channelDateTime" ON "lines" ("channelId","time","date")', callback);
-		db._db.run('CREATE INDEX "usernameDateTime" ON "lines" ("time","date","username")', callback);
-		db._db.run('VACUUM', callback);
+		db._db.run(
+			'DROP INDEX "username"',
+			callback("Drop username index")
+		);
+		db._db.run(
+			'DROP INDEX "date"',
+			callback("Drop date index")
+		);
+		db._db.run(
+			'DROP INDEX "channelId"',
+			callback("Drop channel id index")
+		);
+		db._db.run(
+			'CREATE INDEX "channelDateTime" ON "lines" ("channelId","time","date")',
+			callback("Create index channel date time")
+		);
+		db._db.run(
+			'CREATE INDEX "usernameDateTime" ON "lines" ("time","date","username")',
+			callback("Create index username date time")
+		);
+		db._db.run(
+			'VACUUM',
+			callback("Vacuum")
+		);
 	});
 });
