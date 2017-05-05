3b0300e6cd8bed5917fcbeb8a59fddc53d5fb553
Andy Graulund <andreas@graulund.com>
2017-05-05 10:23:38 +0200

    Warn before deleting everything

--- scripts/clearDatabaseLogs.sh
+++ scripts/clearDatabaseLogs.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 command -v sqlite3 >/dev/null 2>&1 || { echo >&2 "This script requires the sqlite3 command line tool, but it's not installed. Aborting."; exit 1; }
-sqlite3 ../data/pyramid.db < clearDatabaseLogs.sql
+
+echo -n "WARNING: You are about to delete all logs in your Pyramid database. Do you wish to continue? [y/n] "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+	sqlite3 ../data/pyramid.db < clearDatabaseLogs.sql
+fi
--- scripts/clearLogFolder.sh
+++ scripts/clearLogFolder.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-rm -rf ../public/data/logs/*
+#!/bin/bash
+echo -n "WARNING: You are about to delete all logs in your Pyramid log folder. Do you wish to continue? [y/n] "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+	rm -rf ../public/data/logs/*
+fi
