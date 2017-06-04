192123e64a09671fc27ec8936112abdabb0c0936
Andy Graulund <andreas@graulund.com>
2017-06-04 17:36:56 +0200

    Make heapdump optional, and display helpful info if you have it installed

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,21 @@
 // PYRAMID
 
-// For debugging purposes
-const heapdump = require("heapdump"); // eslint-disable-line no-unused-vars
+// DEBUG TEMP START
+
+const optional = require("optional");
+const heapdump = optional("heapdump");
+
+if (heapdump) {
+	console.log(
+		"Heap dumps enabled! If this process is acting up, " +
+		`run:\n\tkill -USR2 ${process.pid}\n` +
+		"And then send the resulting heapdump file to the developer.\n" +
+		"This helps us improve the app.\n\n" +
+		"Thanks for trying out Pyramid!\n"
+	);
+}
+
+// DEBUG TEMP END
 
 // Main app service
 const main = require("./server/main");
