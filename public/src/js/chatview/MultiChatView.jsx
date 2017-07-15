30a072bbdffe351c3ac1a575b6bcf8e17ec5fafd
Andy Graulund <andreas@graulund.com>
2017-07-15 21:32:16 +0200

    Basic multi support

---
+++ public/src/js/chatview/MultiChatView.jsx
@@ -0,0 +1,85 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import ChatView from "./ChatView.jsx";
+import NoChatView from "./NoChatView.jsx";
+
+class MultiChatView extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.renderItem = this.renderItem.bind(this);
+	}
+
+	renderItem(data, index) {
+		let { currentLayoutFocus } = this.props;
+		let focus = currentLayoutFocus === index;
+
+		let styles = [];
+		let content;
+
+		if (data) {
+			let {
+				columnEnd,
+				columnStart,
+				query,
+				rowEnd,
+				rowStart,
+				type
+			} = data;
+
+			if (query && type) {
+				content = (
+					<ChatView
+						focus={focus}
+						pageType={type}
+						pageQuery={query} />
+				);
+			}
+
+			styles = {
+				gridColumn: columnStart && columnEnd &&
+					`${columnStart} / ${columnEnd}`,
+				gridRow: rowStart && rowEnd &&
+					`${rowStart} / ${rowEnd}`
+			};
+		}
+
+		if (!content) {
+			content = <NoChatView focus={focus} />;
+		}
+
+		return (
+			<div className="multichat__item" style={styles} key={index}>
+				{ content }
+			</div>
+		);
+	}
+
+	render() {
+		let { currentLayout } = this.props;
+
+		if (!currentLayout || !currentLayout.length) {
+			return <NoChatView />;
+		}
+
+		return (
+			<div className="multichat">
+				<div className="multichat__inner">
+					{ currentLayout.map(this.renderItem) }
+				</div>
+			</div>
+		);
+	}
+}
+
+MultiChatView.propTypes = {
+	currentLayout: PropTypes.array,
+	currentLayoutFocus: PropTypes.number
+};
+
+export default connect(({
+	viewState: { currentLayout, currentLayoutFocus }
+}) => ({
+	currentLayout, currentLayoutFocus
+}))(MultiChatView);
